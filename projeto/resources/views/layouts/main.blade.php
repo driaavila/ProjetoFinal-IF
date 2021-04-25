f392feaeb648f6b0e546f7a91b4486bec5d3f402
Fabiene <82477238+Fabienepereira@users.noreply.github.com>
2021-04-25 17:21:56 -0300

    css - ok

--- projeto/resources/views/layouts/main.blade.php
+++ projeto/resources/views/layouts/main.blade.php
@@ -12,7 +12,10 @@
 
         <!-- CSS Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
+        
+        <!-- CSS da aplicação -->
+        <link rel="stylesheet" href="/css/style.css">
+        <script src ="/js/scripts.js"></script>   
    
     </head>
     <body>
@@ -20,7 +23,7 @@
          <nav class="navbar navbar-expand-lg navbar-light">
          <div class="collapse navbar-collapse" id="navbar">
          <a href="" class="navbar-brand">
-
+         <img src="/img/logo.png" alt="Restaurante Universitário">
          </a>
          <ul class="navbar-nav">
             <li class="nav-item">
@@ -44,10 +47,19 @@
         <div id="container" class="col-md-12">
             <h1> Restaurante Universitário </h1>
         </div>
-            
+
+        <div class="container-fluid">
+                <div class="row">
+                    @if(session('msg'))
+                        <p class="msg">{{ session('msg') }}</p>
+                    @endif
+                    @yield('content')
+                </div>
+        </div>            
     </main>
     <div class="footer">
     <p>RESTAURANTE UNIVERSITÁRIO &copy; 2021</p>
     </div>
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
     </body>
 </html>
